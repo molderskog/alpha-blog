023f2db4da3f859126d9a63f13fbe0e9b7395c14
Martin Olderskog <molderskog@gmail.com>
2020-06-28 18:28:55 +0200

    Add articles listing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,11 @@
 class ArticlesController < ApplicationController
 
   def show
-    byebug
     @article = Article.find(params[:id]) # ved å gjøre dette til en instansvariabel, så vil article være tilgjengelig for viewet
+  end
+
+  def index
+    @articles = Article.all
   end
 
 end
